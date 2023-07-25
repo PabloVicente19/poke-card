11f38e089f57647e6705c602781b1fd6ad1c504d
Pablo Vicente <pablovicente19@hotmail.com>
2023-07-25 18:18:25 -0300

    trabajar en la logica del renderizado del componente

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,5 +1,19 @@
+import  {useEffect, useState } from "react"
+
+
+
 export default function PokeCard() {
+  const ids = [1,2,3,4,5,6,7,8,9];
+
+  const apiCall = async (id) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json()
+    console.log(data)
+    return data
+  }
+
+
   return (
-    <div className="card-content"><h2>pokemn</h2></div>
+    <div className="card-content">{data?.name}</div>
   )
 }
