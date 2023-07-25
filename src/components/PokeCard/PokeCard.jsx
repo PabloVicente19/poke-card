447b405d6d012412c6218f029e3ff3bf3e94f8bd
Pablo Vicente <pablovicente19@hotmail.com>
2023-07-25 20:34:50 -0300

    renderiza

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,19 +1,71 @@
-import  {useEffect, useState } from "react"
-
-
+import { useEffect, useState } from "react";
+import "./card.css";
 
 export default function PokeCard() {
-  const ids = [1,2,3,4,5,6,7,8,9];
+  const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const apiCall = async (id) => {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await res.json()
-    console.log(data)
-    return data
+    const data = await res.json();
+    return data;
+  };
+
+  const [data, setData] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    apiCall(ids[index])
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(true);
+        // setError(err);
+      });
+  }, [index]);
+
+  const handleIndex = () => {
+    return setIndex(index + 1);
+  };
+
+  if (loading) {
+    return <p>Cargando...</p>;
   }
-
-
+  if (error) {
+    return (
+      <>
+        {error}
+        <button onClick={handleIndex}>Siguiente</button>
+      </>
+    );
+  }
   return (
-    <div className="card-content">{data?.name}</div>
-  )
+    <>
+      {index === ids.length - 1 ? (
+        <>
+          <p className="error-text">No hay mas</p>
+          <button
+            className="btn-reset"
+            onClick={() => {
+              setIndex(1);
+            }}
+          >
+            Reset
+          </button>
+        </>
+      ) : (
+        <div className="card-content">
+          <h2 className="poke-name">{data.name}</h2>
+          <img className="poke-img" src={data.sprites.front_default} />
+
+          <button className="card-btn" onClick={handleIndex}>
+            siguiente
+          </button>
+        </div>
+      )}
+    </>
+  );
 }
